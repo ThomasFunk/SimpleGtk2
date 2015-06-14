da8dc79467425e87b948008b4b54c663ae42d2dd
Thomas Funk <t.funk@web.de>
2015-06-14 22:27:31 +0200

    add 'use strict' to all examples

--- examples/msg_dialog_simple.pl
+++ examples/msg_dialog_simple.pl
@@ -1,7 +1,26 @@
 #!/usr/bin/perl -w
 
-use strict;
+#-----------------------------------------------------------------------
+# Copyright (C) 2015 by Thomas Funk <t.funk@web.de>
+# 
+# This library is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Library General Public License as published by the Free
+# Software Foundation; either version 2.1 of the License, or (at your option)
+# any later version.
+# 
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Library General Public License for
+# more details.
+# 
+# You should have received a copy of the GNU Library General Public License
+# along with this library; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place - Suite 330, Boston, MA  02111-1307  USA.
+#-----------------------------------------------------------------------
+
+
 use SimpleGtk2;
+use strict;
 
 sub nonModal{
     my $response = shift;
@@ -23,27 +42,47 @@ sub Simple {
 }
 
 # Toplevel window
-my $win = SimpleGtk2->new_window(Name => 'mainWindow', Title => 'Message Test', Size => [200, 160]);
+my $win = SimpleGtk2->new_window(Type => 'toplevel',
+                        Name    => 'mainWindow',
+                        Title   => 'Message Test', 
+                        Size    => [200, 160]);
 
 # a modal message dialog
-$win->add_button(Name => 'Button1', Pos => [60, 10], Size => [80, 40], Title => "_Modal");
+$win->add_button(Name => 'Button1',
+        Pos     => [60, 10],
+        Size    => [80, 40],
+        Title   => "_Modal");
 $win->add_signal_handler('Button1', 'clicked', sub{\&Modal($win);});
 
-$win->add_msg_dialog(Name => 'diag1', DType => 'ok-cancel', MType => 'warning', Icon => 'gtk-quit');
+$win->add_msg_dialog(Name => 'diag1',
+        DType   => 'ok-cancel',
+        MType   => 'warning',
+        Icon    => 'gtk-quit');
 
 # a non-modal message dialog
 my $FirstMsg = "<span foreground=\"blue\" size=\"x-large\">Message Type</span>";
 my $SecondMsg = "<span foreground='red' size=\"small\" style ='italic'>Info box.</span>";
 
-$win->add_button(Name => 'Button2', Pos => [60, 60], Size => [80, 40], Title => "_NonModal");
+$win->add_button(Name => 'Button2',
+        Pos     => [60, 60],
+        Size    => [80, 40],
+        Title   => "_NonModal");
 $win->add_signal_handler('Button2', 'clicked', sub{$win->show_msg_dialog('diag2', $FirstMsg, $SecondMsg);});
 
-$win->add_msg_dialog(Name => 'diag2', DType => 'yes-no', MType => 'info', RFunc => \&nonModal, Modal => 0);
+$win->add_msg_dialog(Name => 'diag2',
+        DType   => 'yes-no',
+        MType   => 'info',
+        RFunc   => \&nonModal,
+        Modal   => 0);
 
 # a simple message dialog
-$win->add_button(Name => 'Button3', Pos => [60, 110], Size => [80, 40], Title => "_Simple");
+$win->add_button(Name => 'Button3',
+        Pos     => [60, 110],
+        Size    => [80, 40],
+        Title   => "_Simple");
 $win->add_signal_handler('Button3', 'clicked', sub{\&Simple($win);});
 
-$win->show_all();
+$win->show_and_run();
+
 
 
--- examples/notebook_simple.pl
+++ examples/notebook_simple.pl
@@ -1,34 +1,104 @@
 #!/usr/bin/perl -w
 
+#-----------------------------------------------------------------------
+# Copyright (C) 2015 by Thomas Funk <t.funk@web.de>
+# 
+# This library is free software; you can redistribute it and/or modify it under
+# the terms of the GNU Library General Public License as published by the Free
+# Software Foundation; either version 2.1 of the License, or (at your option)
+# any later version.
+# 
+# This library is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Library General Public License for
+# more details.
+# 
+# You should have received a copy of the GNU Library General Public License
+# along with this library; if not, write to the Free Software Foundation, Inc.,
+# 59 Temple Place - Suite 330, Boston, MA  02111-1307  USA.
+#-----------------------------------------------------------------------
 
-use strict;
+
 use SimpleGtk2;
+use strict;
 
 # Toplevel window
-my $win = SimpleGtk2->new_window(Name => 'mainWindow', Title => 'Notebook simple', Size => [400, 400],
-                                Fixed => 0, ThemeIcon => 'emblem-dropbox-syncing');
+my $win = SimpleGtk2->new_window(Type => 'toplevel',
+                        Name        => 'mainWindow',
+                        Title       => 'Notebook simple', 
+                        Size        => [400, 400],
+                        Fixed       => 0,
+                        ThemeIcon   => 'emblem-dropbox-syncing');
 
 # notebook
-$win->add_notebook(Name => 'NB1', Pos => [10, 10], Size => [200, 200], Tabs => 'top', scroll => 1, Popup => 1);
+$win->add_notebook(Name => 'NB1',
+        Pos     => [10, 10],
+        Size    => [300, 300], 
+        Tabs    => 'top', 
+        scroll  => 1, 
+        Popup   => 1);
 
-$win->add_nb_page(Name => "NB_page1", Pos_n => 0, Title => "0Sabbel", Notebook => 'NB1', Tip => "Blafasel_1");
-$win->add_nb_page(Name => "NB_page2", Pos_n => 1, Title => "1Babbel", Notebook => 'NB1', Tip => "Blafasel_2");
-$win->add_nb_page(Name => "NB_page3", Pos_n => 3, Title => "3Cabbel", Notebook => 'NB1', Tip => "Blafasel_3");
-$win->add_nb_page(Name => "NB_page4", Pos_n => 2, Title => "2Dabbel", Notebook => 'NB1', Tip => "Blafasel_4");
-$win->add_nb_page(Name => "NB_page5", Pos_n => 4, Title => "4Eabbel", Notebook => 'NB1', Tip => "Blafasel_5");
-$win->add_nb_page(Name => "NB_page6", Pos_n => 5, Title => "5Fabbel", Notebook => 'NB1', Tip => "Blafasel_6");
-$win->add_nb_page(Name => "NB_page7", Pos_n => 6, Title => "6Gabbel", Notebook => 'NB1', Tip => "Blafasel_7");
-
-$win->add_button(Name => 'Button', Pos => [10, 345], Size => [80, 40], Title => "_Jup", Tip => 'Bla fasel', Frame => 'NB_page1');
+$win->add_nb_page(Name => "NB_page1", 
+        Pos_n   => 0, 
+        Title   => "0Sabbel", 
+        Notebook => 'NB1', 
+        Tip     => "Blafasel_1");
 # deactivate first page
 $win->set_sensitive("NB_page1", 0);
+
+$win->add_nb_page(Name => "NB_page2",
+        Pos_n   => 1,
+        Title   => "1Babbel",
+        Notebook => 'NB1',
+        Tip     => "Blafasel_2");
 # change title in second page
 $win->set_title("NB_page2", "1Bibbile");
 
-$win->add_button(Name => 'removeButton', Pos => [310, 345], Title => "_Remove", Tip => 'Click removes page 6');
+$win->add_nb_page(Name => "NB_page3",
+        Pos_n   => 3,
+        Title   => "3Cabbel",
+        Notebook => 'NB1',
+        Tip     => "Blafasel_3");
+
+$win->add_nb_page(Name => "NB_page4",
+        Pos_n   => 2,
+        Title   => "2Dabbel",
+        Notebook => 'NB1', 
+        Tip     => "Blafasel_4");
+
+$win->add_nb_page(Name => "NB_page5", 
+        Pos_n   => 4,
+        Title   => "4Eabbel",
+        Notebook => 'NB1',
+        Tip     => "Blafasel_5");
+$win->set_font('NB_page5', Family => 'Bernard MT Condensed');
+$win->set_font_color('NB_page5','red');
+
+$win->add_nb_page(Name => "NB_page6",
+        Pos_n   => 5,
+        Title   => "5Fabbel",
+        Notebook => 'NB1',
+        Tip     => "Blafasel_6");
+$win->add_nb_page(Name => "NB_page7",
+        Pos_n   => 6, 
+        Title   => "6Gabbel",
+        Notebook => 'NB1',
+        Tip     => "Blafasel_7");
+
+$win->add_button(Name => 'Button',
+        Pos => [10, 345],
+        Size => [80, 40],
+        Title => "_Jup",
+        Tip => 'Bla fasel',
+        Frame => 'NB_page1');
+
+$win->add_button(Name => 'removeButton',
+        Pos => [310, 345],
+        Title => "_Remove",
+        Tip => 'Click removes page 6');
 $win->add_signal_handler('removeButton', 'clicked', sub{$win->remove_nb_page('NB_page7');});
 
-$win->show_all();
+$win->show_and_run();
 
 
  
